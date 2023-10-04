2f1e9ec00515b69728664dd5ed36379b29e0665d
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2023-10-04 09:30:08 -0300

    Hello World

---
+++ PROGCOB01.cob
@@ -0,0 +1,13 @@
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. PROGCOB01.
+      **************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTOR = THIAGO(SOUZA) - THIAGOS
+      * OBJETIVO: MOSTRAR A STRING HELLO WORLD
+      * DATA = XX/XX/XXXX
+      **************************************
+        ENVIRONMENT DIVISION.
+        DATA DIVISION.
+        PROCEDURE DIVISION.
+            DISPLAY 'HELLO WORLD'.
+            STOP RUN.
