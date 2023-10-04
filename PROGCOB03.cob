8e65c3f62efd58210a7a3278231a11e9e18ea009
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2023-10-04 09:31:29 -0300

    Data

---
+++ PROGCOB03.cob
@@ -0,0 +1,20 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB02.
+      **************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTOR = THIAGO(SOUZA) - THIAGOS
+      * OBJETIVO: RECEBER E IMPRIMIR A DATA DO SISTEMA
+      * UTILIZAR AS VARIAVEIS COM NIVEL 01.02...(ESTRUTURADA)
+      * DATA = XX/XX/XXXX
+      **************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WRK-DATA.
+           02 WRK-ANO PIC 9(04) VALUE ZEROS.
+           02 WRK-MES PIC 9(02) VALUE ZEROS.
+           02 WRK-DIA PIC 9(02) VALUE ZEROS.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-DATA FROM DATE YYYYMMDD.
+           DISPLAY 'DATA: ' WRK-DIA ' DE ' WRK-MES ' DE ' WRK-ANO.
+           STOP RUN.
