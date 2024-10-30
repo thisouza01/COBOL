6be4db4be45f1ef356efb1ccd635de3d754edb71
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2024-10-30 10:22:03 -0300

    Update inventario.cbl
    Adicionando função de verificação de urgência para produtos com estoque abaixo do mínimo

--- inventario.cbl
+++ inventario.cbl
@@ -1,7 +1,10 @@
       ******************************************************************
       * Author:
       * Date:
-      * Purpose:
+      * Purpose: Ler um arquivo sequencial de inventário, processar os
+      *  dados e gerar um relatório com produtos que estão abaixo do 
+      *  estoque mínimo e também listar produtos que precisam ser 
+      *  repostos com urgência.
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
@@ -37,6 +40,7 @@
 
            01 AUX.
                05 EOF                  PIC X(01) VALUE 'N'.
+               05 FLAG-STQ-MIN         PIC 9(01) VALUE 0.
 
            01 WS-STATUS.
                05 FS-INVENT            PIC 9(02).
@@ -49,6 +53,7 @@
                IF EOF = 'N'
                    PERFORM 0300-UNSTRING-ARQUIVO
                    PERFORM 0400-IDENTIFICA-STQ-MIN
+                   PERFORM 0410-IDENTIFICA-URGENCIA
                END-IF
            END-PERFORM.
            PERFORM 1000-FECHA-ARQUIVO.
@@ -81,16 +86,43 @@
            MOVE UNS-QNT_ATUAL TO QNT_ATUAL.
            MOVE UNS-STQ_MIN TO STQ_MIN.
            MOVE UNS-STQ_MAX TO STQ_MAX.
-           DISPLAY WS-REGISTRO.
+           PERFORM 0450-MOSTRA-REGISTRO.
 
        0400-IDENTIFICA-STQ-MIN.
            IF UNS-QNT_ATUAL < UNS-STQ_MIN
                PERFORM 0310-MOVE-REGISTRO
+               MOVE 1 TO FLAG-STQ-MIN
+           ELSE
+               MOVE 0 TO FLAG-STQ-MIN
            END-IF.
+               
+       0410-IDENTIFICA-URGENCIA.
+           IF FLAG-STQ-MIN = 1
+      *QUANDO FOR MENOR QUE A METADE DO ESTOQUE MAXIMO
+      *OU MAIOR QUE 20% DO 0 
+               IF UNS-QNT_ATUAL > (UNS-STQ_MAX * 0.5)
+                   DISPLAY 'CONTEM PRODUTOS ACIMA DA METADE DO ESTOQUE'
+               ELSE IF UNS-QNT_ATUAL > 20 AND < (UNS-STQ_MAX * 0.5)
+                   DISPLAY 'PRECISA DE REPOSICAO'
+                   DISPLAY '--/-/-/--'
+               ELSE
+                   DISPLAY 'REPOSICAO URGENTE'
+                   DISPLAY '--/-/-/--'                   
+               END-IF
+           ELSE
+               DISPLAY 'PROD_ID: 'UNS-PROD_ID
+               DISPLAY 'PROD_NOME: 'UNS-PROD_NOME
+               DISPLAY 'PRODUTO DENTRO DO ESTOQUE NORMAL'
+               DISPLAY '-/-/-/-/-'
+           END-IF.    
 
        0450-MOSTRA-REGISTRO.
-           DISPLAY WS-REGISTRO.
-
+           DISPLAY 'PROD_ID: 'PROD_ID.
+           DISPLAY 'PROD_NOME: 'PROD_NOME.
+           DISPLAY 'QNT_ATUAL: 'QNT_ATUAL.
+           DISPLAY 'STQ_MIN: 'STQ_MIN.
+           DISPLAY 'STQ_MAX: 'STQ_MAX.
+           
        1000-FECHA-ARQUIVO.
            CLOSE INVENTARIO.
 
