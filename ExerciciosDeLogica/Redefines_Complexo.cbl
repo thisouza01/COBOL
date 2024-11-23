3c4ebf4bd7a4d421cd49e8c55f70ce8fcd4af831
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2024-11-22 21:53:24 -0300

    Update Redefines_Complexo.cbl

--- ExerciciosDeLogica/Redefines_Complexo.cbl
+++ ExerciciosDeLogica/Redefines_Complexo.cbl
@@ -1,10 +1,10 @@
       ******************************************************************
       * Author:
       * Date:
-      * Purpose:Crie uma estrutura de dados com uma área de 20 bytes
-      *  que contém 4 variáveis diferentes. Use REDEFINES para mapear
-      *  essas variáveis de diferentes formas e observe o impacto de
-      *  manipulações de dados
+      * Purpose:Crie uma estrutura de dados com uma Ã¡rea de 20 bytes
+      *  que contÃ©m 4 variÃ¡veis diferentes. Use REDEFINES para mapear
+      *  essas variÃ¡veis de diferentes formas e observe o impacto de
+      *  manipulaÃ§Ãµes de dados
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
@@ -12,16 +12,25 @@
       *
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+      * AnotaÃ§Ãµes
+      * Considerei o exemplo 
+      * A  pic x(01).
+      *  B  REDEFINES a
+      *    B1 OCCURS tantas vezes
+      *     
+      *  C  REDEFINES a  
+      *    C1 OCCURS tantas vezes
+      * 
       *
        01  DADOS.
       * Valor que vai entrar na variavel
            05 VALOR-BRUTO      PIC X(40).
-      * Valores alfabeticos serão mostrados
+      * Valores alfabeticos serÃ£o mostrados
            05 NOMES            REDEFINES VALOR-BRUTO.
                10 NOME         PIC X(10) OCCURS 4 TIMES.
       * Valores numericos serao mostrados
            05 NUMERICOS        REDEFINES VALOR-BRUTO.
-               10 NUMERO       PIC 9(20) OCCURS 2 TIMES.
+               10 NUMERO       PIC 9(10) OCCURS 2 TIMES.
 
        01  INDICE.
            05 I                PIC 9(02) VALUE 1.
@@ -29,19 +38,22 @@
       *
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-           PERFORM MOVE-DADOS.
+           PERFORM MOVE-DADOS1.
            PERFORM PERCORRE-NOMES.
+           PERFORM MOVE-DADOS2.
            PERFORM PERCORRE-VALORES.
            STOP RUN.
 
-       MOVE-DADOS.
+       MOVE-DADOS1.
            MOVE 'MARIA     JOAO     ANA      LUCAS' TO VALOR-BRUTO.
-           MOVE '1234567890' TO VALOR-BRUTO(31:).
-
+          
        PERCORRE-NOMES.
            PERFORM VARYING I FROM 1 BY 1 UNTIL I > 4
                DISPLAY 'Nome ' I ': ' NOME(I)
            END-PERFORM.
+
+       MOVE-DADOS2.
+           MOVE '12345678901234567890' TO VALOR-BRUTO.
 
        PERCORRE-VALORES.
            PERFORM VARYING J FROM 1 BY 1 UNTIL J > 2
